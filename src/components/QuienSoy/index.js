32c22b1c190b2fa58f11d80a382c0978fb437f5c
Cristian de la Sota <cristiandelasota@gmail.com>
2019-02-03 10:17:32 -0300

    Arreglo texto QuienSoy

--- src/components/QuienSoy/index.js
+++ src/components/QuienSoy/index.js
@@ -10,11 +10,11 @@ const QuienSoy = () => {
         Cristian de la Sota
       </p>
       <p className="subheading">
-        Hola! Soy Cristan. Soy Ingeniero en Sistemas (UAI 2014) y vivo en San Nicolas, Buenos Aires, Argentina.<br />
+        Hola! Soy Cristan. Soy Ingeniero en Sistemas (UAI 2014) y vivo en San Nicolás, Buenos Aires, Argentina.<br />
         Soy desarrollador ABAP Certificado y tengo experiencia en SAP PI, Fiori, UI5 y otras tecnologías dentro de SAP.
         Mi nivel de inglés es intermedio.<br /> Actualmente estoy incursionando en el desarrollo de aplicaciones web y
         tratando de convertirme en desarrollador full stack. También me interesan las tecnologías emergentes como IoT
-        y Blockchain donde he realizados varias PoC en la red de Ethereum.
+        y Blockchain donde realicé varias PoC en la red de Ethereum.
       </p>
     </div>
   )
